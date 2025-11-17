4c75a549ae9e5952eb783cf26703f9f135ef257f
AlessandroTelesca <alessandro.telesca03@gmail.com>
2025-11-17 16:12:34 +0100

    Add Angular component for map visualization. Modify Angular routing for the components.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-import { RobotVisualizerComponent } from './robot-visualizer/robot-visualizer.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RobotVisualizerComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'frontend';
+  title = 'KVV-Leitstandsoftware';
 }
---
+++ frontend/src/app/map-visualizer/map-visualizer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-map-visualizer',
+  standalone: true,
+  templateUrl: './map-visualizer.component.html',
+  styleUrls: ['./map-visualizer.component.css']
+})
+export class MapVisualizerComponent {
+
+}
