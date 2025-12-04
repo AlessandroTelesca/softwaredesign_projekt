a1460a38afe6d49bad66b27333aaebc7d43b2213
Daniel B. Georg <124447854+SootDan@users.noreply.github.com>
2025-12-04 14:58:48 +0100

    rm repo in repo

--- softwaredesign_projekt-main/backend/README.md
+++
@@ -1,7 +0,0 @@
-# Backend
-
-Python with Flask. Database with SQLite.
-
-app.py: Main Flask app.  
-database.py: Concerning SQLite.  
-geography.py: Map generation and handling.
--- softwaredesign_projekt-main/frontend/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { RobotVisualizerComponent } from './robot-visualizer/robot-visualizer.component';
-import { MapVisualizerComponent } from './map-visualizer/map-visualizer.component';
-
-export const routes: Routes = [
-	{ path: '', redirectTo: 'robot', pathMatch: 'full' },
-	{ path: 'robot', component: RobotVisualizerComponent },
-	{ path: 'map', component: MapVisualizerComponent },
-];
